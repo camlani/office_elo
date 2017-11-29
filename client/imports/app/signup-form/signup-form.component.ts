8cb8097ca6ed60f10e2164e62d976953d25847ba
camlani <Cameron@Camerons-MacBook-Pro.local>
2017-11-28 23:09:55 -0500

    updating packages hopefully solving issues

--- client/imports/app/signup-form/signup-form.component.ts
+++ client/imports/app/signup-form/signup-form.component.ts
@@ -3,6 +3,7 @@ import { Observable, Subscription } from 'rxjs';
 import { InjectUser } from 'angular2-meteor-accounts-ui';
 
 import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
 import { MeteorObservable } from 'meteor-rxjs';
 
 @Component({
@@ -14,7 +15,34 @@ import { MeteorObservable } from 'meteor-rxjs';
   @InjectUser('user')
   export class SignupFormComponent implements OnInit {
     user: Meteor.User;
+    displayName: string;
+    email: string;
+    password: string;
     ngOnInit() {
       
     }
+    /**
+     * Submit User Password
+     */
+    submitSignUpUserPassword() {
+      console.log("Userpass " + this.email + " " + this.password )
+
+      var options = {
+        username: this.displayName,
+        email: this.email,
+        password: this.password
+      }
+
+      Accounts.createUser(options, (error)=> {
+        if ( error ) {
+          console.log("Not logged in")
+        } else {
+          console.log("Logged in")
+          //need to redirect here
+        }
+
+      });
+
+  }
+
 }  
--- server/main.ts
+++ server/main.ts
@@ -25,4 +25,24 @@ Meteor.startup(() => {
 
 });
 
+Accounts.onCreateUser( ( options, user ) => {
+    let profile = options.profile;
+ 
+    if(!user.services.google){
+        if ( profile ) {
+            user.profile = profile;
+        }
+    
+    } else {
+        const {name} = user.services.google;
+        user.username = name;
+    }
+
+
+    user._id = Random.id();
+
+    return user;
+  });
+
+
 //Need to finish main.ts
