fb5cbaed1cfdabe8b2f9038c897e92c3c7a566f6
loftonanderson <lja5750@uncw.edu>
2017-12-05 19:17:04 -0500

    Basic "Games Tracked" feature on homepage
    Games Tracked is now live - working implementation, will be improved
    next commit

--- client/imports/app/home-page/home-page.component.ts
+++ client/imports/app/home-page/home-page.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router'; 
+import { Observable, Subscription } from 'rxjs';
 
 import { Meteor } from 'meteor/meteor';
+import { MeteorObservable } from 'meteor-rxjs';
+
+
+// To delete after correctly implemented
+import { MatchStats } from '../../../../imports/collections/matchstats';
+import { MatchStat } from '../../../../imports/models/matchstat';
 
 @Component({
     selector: 'home-page',
@@ -10,12 +16,30 @@ import { Meteor } from 'meteor/meteor';
     styleUrls: ['home-page.scss']
   })
   export class HomePageComponent {
+    matchstats: Observable<MatchStat[]>;
+    matchListSubscription: Subscription;
+    matchCountNum: number;
 
     searchValue:string;
 
     constructor(
       private router: Router
     ) { }
+
+    ngOnInit() {
+      this.matchListSubscription = MeteorObservable.subscribe('matchList').subscribe(() => {
+        this.matchstats = MatchStats.find();   
+
+        MatchStats.find().map(matches => matches.length)
+        .subscribe(count => this.matchCountNum = count)
+      });
+    }
+
+    ngOnDestroy() {
+      if (this.matchListSubscription) {
+        this.matchListSubscription.unsubscribe();
+      }
+    }
 
     goToUserProfile() {
       if (this.searchValue != null){
