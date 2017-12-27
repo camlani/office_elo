411d71769de1744409bb5558e7128f2ebf031cd1
camlani <Cameron@Camerons-MacBook-Pro.local>
2017-12-27 10:07:29 -0500

    Added in Delete Match functionality from Admin Page
    Added Delete Match Functionality form the Admin Page

--- client/imports/app/admin-page/admin-page.component.ts
+++ client/imports/app/admin-page/admin-page.component.ts
@@ -1,9 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+
 import { InjectUser } from 'angular2-meteor-accounts-ui';
 
 import { Roles } from 'meteor/alanning:roles';
 
 import { Meteor } from 'meteor/meteor';
+import { MeteorObservable } from 'meteor-rxjs';
+
+import { MatchStats } from '../../../../imports/collections/matchstats';
+import { MatchStat } from '../../../../imports/models/matchstat';
+
 
 @Component({
     selector: 'admin-page',
@@ -13,11 +20,27 @@ import { Meteor } from 'meteor/meteor';
 @InjectUser('user')
   export class AdminPageComponent implements OnInit{
     user: Meteor.User;
-
+    matchstats: Observable<MatchStat[]>;
+    matchListSubscription: Subscription;
+    editMatches = false;
     ngOnInit(){
-        //console.log(Meteor)
+      this.matchListSubscription = MeteorObservable.subscribe('matchList').subscribe(() => {
+        this.matchstats = MatchStats.find({},{sort:{mTime:-1}});
+      });
     }
-
+    ngOnDestroy() {
+      if (this.matchListSubscription) {
+        this.matchListSubscription.unsubscribe();
+      }
+    }
+    removeMatchStat(_id: string) {
+      if(Roles.userIsInRole(Meteor.userId(),['super-admin'])){
+        Meteor.call('removeMatchStats', _id);
+      }
+    }
+    clickEditMatches(){
+      this.editMatches = true;
+    }
     //Need to have the add new location drop down
     //then add the slider etc.
 
